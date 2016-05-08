2ca90d45c8297c9ca2152ebbae2ed2ce315e73ac
Lior <lior@macbook-pro-2.local>
2016-05-08 19:13:40 +0300

    Initial Commit

---
+++ AVAudioSessionWorkaround/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVAudioSessionWorkaround
+//
+//  Created by Lior on 08/05/2016.
+//  Copyright © 2016 Azi Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
